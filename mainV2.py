7aa995dfb2e868f0765604c8fdc1da819b0d3eea
scambust <104972881+scambust@users.noreply.github.com>
2022-05-05 13:28:41 -0500

    Add files via upload

---
+++ mainV2.py
@@ -0,0 +1,10 @@
+import keyboard as keyboard
+
+# Record events until 'esc' is pressed.
+recorded = keyboard.record(until='esc')
+# while loop and counter replaced
+# for loop repeats the message 10000x
+for i in range(10000):
+    # playback at 100x speed
+    keyboard.play(recorded, speed_factor=100)
+    print(i)
